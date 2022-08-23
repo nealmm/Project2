e676ab9fc245ba5c723f7f2da640eaf99aec3ee7
Michael Park <tjp4ca@gmail.com>
2022-08-23 00:39:33 -0400

    changed routes to controllers

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
+// matt, i renamed routes folder to controllers
+
 const express = require('express');
-const routes = require('./routes');
+// const routes = require('./routes');
+const controllers = require('./controllers');
 const sequelize = require('./config/connection');
 
 const app = express();
@@ -8,7 +11,8 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(routes);
+// app.use(routes);
+app.use(controllers);
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
