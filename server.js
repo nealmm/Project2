323b186ef2fcdcb1022a467ad30abfa00378ccd2
Michael Park <tjp4ca@gmail.com>
2022-08-27 13:00:51 -0400

    session added

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
 const path = require('path');
 const express = require('express');
 
+const session = require('express-session');
+
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 
-const controllers = require('./controllers');
-const sequelize = require('./config/connection');
-
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const controllers = require('./controllers');
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'diningSecret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
 
 // handlebar engine
 app.engine('handlebars', hbs.engine);
