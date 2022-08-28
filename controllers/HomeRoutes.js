a04e6639441dfdb88ba23d8e5698b88a3dec44c8
Michael Park <tjp4ca@gmail.com>
2022-08-27 20:44:13 -0400

    modular home handlebar

--- controllers/HomeRoutes.js
+++ controllers/HomeRoutes.js
@@ -2,15 +2,65 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { User, Dining, Review } = require('../models');
 
-// handlebar routes
+// render homepage handlebar
 router.get('/', (req, res) => {
     // res.send('hello world!')
-    res.render('homepage', { title: 'Home Page' });
+    res.render('homepage', { 
+        // title: 'Home Page'
+    
+        bio: [
+            {
+                name: 'Michael Park',
+                fORb: 'Back End Development',
+                icon: '/assets/images/tjp4ca.png'
+            },
+            {
+                name: 'Matt Neal',
+                fORb: 'Back End Development',
+                icon: '/assets/images/nealmm.png'
+            },
+            {
+                name: 'Franco Melendez',
+                fORb: 'Front End Development',
+                icon: '/assets/images/alenco98.png'
+            },
+            {
+                name: 'Graham Smith',
+                fORb: 'Front End Development',
+                icon: '/assets/images/gsmith560.png'
+            }
+        ],
+
+        purpose: [
+            {
+                h2: 'Dine',
+                p: 'Dine at local Washington DC restaurants'
+            },
+            {
+                h2: 'Review',
+                p: 'Post your reviews on this secure forum for members to see'
+            },
+            {
+                h2: 'Share',
+                p: 'Communicate with other community members and share recommendations'
+            },
+            {
+                h2: 'Explore',
+                p: 'Discover new favorite restaurants'
+            }
+        ]
+
+    });
 });
 
-// app.get('/login', (req, res) => {
-//     res.render('login', { layout: 'chooseDifferentLayoutFromLayouts' });
-// });
+router.get('/login', (req, res) => {
+    // if (req.session.loggedIn) {
+    //     res.redirect('/');
+    //     return;
+    // }
+
+    res.render('login', { layout: 'secondary' });
+});
 
 // *** add other routes later
 
